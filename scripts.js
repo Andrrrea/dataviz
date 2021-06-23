e89e050b49369cf2701f9e7630a07c8999b15abe
Andrrrea <45129542+Andrrrea@users.noreply.github.com>
2021-06-23 16:10:07 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
- // JSON für Highcharts einlesen – AJAX Einladen der JSON Datei – clientside !FUNKTIONIERT! 
+// JSON für Highcharts einlesen – AJAX Einladen der JSON Datei – clientside !FUNKTIONIERT!
 window.onload = AJAX_CRE_Daten;
-  function AJAX_CRE_Daten() {     
-var req = new XMLHttpRequest();      
+function AJAX_CRE_Daten() {    
+var req = new XMLHttpRequest();    
 req.onreadystatechange = function () 
-{         if (req.readyState == 4) 
-{             if (req.status == 200 || req.status == 0) 
-{                 var res = req.responseText;                 
-var jsonobj = JSON.parse(res);             } 
-else alert("Fehler: !!!!!!!!!!!!!!!!!! " + req.statusText);         
-}     
-}      
-req.open("GET", "CRE_Daten.json", true);     
+{        if (req.readyState == 4) 
+{            if (req.status == 200 || req.status == 0) 
+{                var res = req.responseText;                
+var jsonobj = JSON.parse(res);            } 
+else alert("Fehler: !!!!!!!!!!!!!!!!!! " + req.statusText);        
+}    
+}    
+req.open("GET", "CRE_Daten.json", true);    
 req.send();
- } 
+}
